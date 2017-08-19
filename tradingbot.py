1b7408bda168f128bd10cbcdd66e90b826eae295
BatuhanUsluel <Batuhan.Usluel@gmail.com>
2017-08-19 18:53:11 -0400

    Added order size calculation when balance not enough, added more comments to code, fixed typo

--- tradingbot.py
+++ tradingbot.py
@@ -6,6 +6,8 @@ import time
 import sys
 from poloniex import poloniex
 def main(argv):
+
+	#Inputs and set variables
 	period = float(raw_input("Period(Delay Between Each Check in seconds):	"))
 	currency = raw_input("Coin(Example: ETH):	")
 	minArb=float(raw_input("Minimum Arbitrage %. Recomended to set above 100.5 as fees from both sides add up to 0.5%.	"))
@@ -13,7 +15,7 @@ def main(argv):
 	tradePlaced = False
 
 	#Bittrex API Keys
-	api = bittrex('BITTREXAPIKEYHERE','BITTREXAPISECRETHERE')
+	api = bittrex('BITTREXAPIKEY','BITTREXAPISECRET')
 
 	#Bittrex market
 	market= '{0}-{1}'.format(trade,currency)
@@ -25,6 +27,7 @@ def main(argv):
 	conn= poloniex('POLONIEXAPIKEY','POLONIEXAPISECRET')
 
 	while True:
+
 		#Poloniex Prices
 		currentValues = conn.api_query("returnTicker")
 		poloBid = float(currentValues[pair]["highestBid"])
@@ -41,42 +44,62 @@ def main(argv):
 
 		#Balances for currency
 		bittrexBalance=api.getbalance(currency)
-		allpolobalance=conn.apiquery('returnBalances')
+		allpolobalance=conn.api_query('returnBalances')
 		poloniexBalance=allpolobalance[currency]
 
 
 		#Balances for BTC
-		bittrexBTCBalance=api.getbalance(btc)
-		poloniexBTCBalance=allpolobalance[btc]
+		bittrexBTCBalance=api.getbalance("btc")
+		poloniexBTCBalance=allpolobalance["BTC"]
 
-
+		#Buy from Polo, Sell to Bittrex
 		if (poloAsk<bittrexBid):
 			arbitrage=bittrexBid/poloAsk
+			#Check if min arb is met
 			if ((arbitrage*100)>minArb):
 				print "Buy from poloAsk, sell to bittrexBid. Profit: " + str(arbitrage*100)
 				sellbook=conn.returnOrderBook(pair)["asks"][0][1]
 				buybook=api.getorderbook(market, "sell")[0]["Quantity"]
-				lowestbook=min(sellbook, buybook)
-				#polocost=poloAsk*sellbook
-				api.selllimit(market, lowestbook, bittrexBid)
-				orderNumber=conn.sell(pair, poloAsk, lowestbook)
-				print "Selling {0} {1} @ BittrexBid @ {2} and buying {3} {4} @ PoloAsk @ {5}".format(lowestbook, currency, bittrexBid, lowestbook, currency, poloAsk)
 
-			#else:
-			#print "Arbitrage found but less than min arb."
+				#Find minimum order size
+				tradesize=min(sellbook, buybook)
+
+				#Setting order size incase balance not enough
+				if (bittrexBalance<tradesize):
+					tradesize=bittrexBalance
+
+				if ((tradesize*poloAsk)>poloniexBTCBalance):
+					tradesize=poloniexBTCBalance/poloAsk
+
+				#Execute order
+				api.selllimit(market, tradesize, bittrexBid)
+				orderNumber=conn.sell(pair, poloAsk, tradesize)
+				print "Selling {0} {1} @ BittrexBid @ {2} and buying {3} {4} @ PoloAsk @ {5}".format(tradesize, currency, bittrexBid, tradesize, currency, poloAsk)
+
+		#Sell to polo, Buy from Bittrex
 		elif(bittrexAsk<poloBid):
 			arbitrage=poloBid/bittrexAsk
+			#Check if min arb is met
 			if ((arbitrage*100)>minArb):
 				print "Buy from Bittrex Ask, sell to poloBid. Profit: " + str(arbitrage*100)
 				buybook=conn.returnOrderBook(pair)["bids"][0][1]
 				sellbook=api.getorderbook(market, "sell")[0]["Quantity"]
-				lowestbook=min(sellbook, buybook)
-				api.buylimit(market, lowestbook, bittrexAsk)
-				orderNumber=conn.sell(pair, poloBid, lowestbook)
-				print "Selling {0} {1} @ PoloBid @ {2} and Buying {3} {4} @ BittrexAsk @ {5}".format(lowestbook, currency, poloBid, lowestbook, currency, bittrexAsk)
 
-#			else:
-#				print "Arbitrage found but less than min arb."
+				#Find minimum order size
+				tradesize=min(sellbook, buybook)
+
+				#Setting order size incase balance not eough
+				if (poloniexBalance<tradesize):
+					tradesize=poloniexBalance
+
+				if((tradesize*bittrexAsk)>bittrexBTCBalance):
+					tradesize=bittrexBTCBalance/bittrexAsk
+
+				#Execute order
+				api.buylimit(market, tradesize, bittrexAsk)
+				orderNumber=conn.sell(pair, poloBid, tradesize)
+				print "Selling {0} {1} @ PoloBid @ {2} and Buying {3} {4} @ BittrexAsk @ {5}".format(tradesize, currency, poloBid, tradesize, currency, bittrexAsk)
+
 		time.sleep(period)
 
 
