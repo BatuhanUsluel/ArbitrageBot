2c6627d1c3040df3b6b1ab1f2b57a9db5793fe88
MrBobBarker <MrBobBarker@users.noreply.github.com>
2017-08-22 09:49:26 -0700

    added trade function to reduce replicated code, renamed variables for consistency

--- tradingbot.py
+++ tradingbot.py
@@ -17,9 +17,9 @@ def main(argv):
 
 	#Inputs and set variables
 	period = float(raw_input("Period(Delay Between Each Check in seconds): "))
-	currency = raw_input("Coin (Example: ETH): ")
+	targetCurrency = raw_input("Coin (Example: ETH): ")
 	minArb = float(raw_input("Minimum Arbitrage % (Recomended to set above 100.5 as fees from both sides add up to 0.5%): "))
-	trade = 'BTC'
+	baseCurrency = 'BTC'
 	tradePlaced = False
 
 	#Bittrex API Keys
@@ -28,23 +28,71 @@ def main(argv):
 	#Polo API Keys
 	poloniexAPI = poloniex('APIKEY','APISECRET')
 
-	#Bittrex market
-	market = '{0}-{1}'.format(trade,currency)
+	# Pair Strings for accessing API responses
+	bittrexPair = '{0}-{1}'.format(baseCurrency,targetCurrency)
+	poloniexPair = '{0}_{1}'.format(baseCurrency,targetCurrency)
 
-	#Polo market
-	pair = '{0}_{1}'.format(trade,currency)
+	# Trade Function
+	def trade(_buyExchange, _ask, _bid, _srcBalance, _buyBalance):
+		# _buyExchange:
+		# 0 = Poloniex
+		# 1 = Bittrex
+
+		arbitrage = (_bid/_ask) * 100
+		# Return minumum arbitrage percentage is not met
+		if ((arbitrage) <= minArb):
+			return
+
+		if (_buyExchange == 0):
+			buyExchangeString = 'Poloniex'
+			sellExchangeString = 'Bittrex'
+			sellbook = poloniexAPI.returnOrderBook(poloniexPair)["asks"][0][1]
+			buybook = bittrexAPI.getorderbook(bittrexPair, "sell")[0]["Quantity"]
+		elif (_buyExchange == 1):
+			buyExchangeString = 'Bittrex'
+			sellExchangeString = 'Poloniex'
+			buybook = poloniexAPI.returnOrderBook(poloniexPair)["bids"][0][1]
+			sellbook = bittrexAPI.getorderbook(bittrexPair, "sell")[0]["Quantity"]
+
+		print('Buy from ' + buyExchangeString + ', sell to ' + sellExchangeString + '. Arbitrage Rate: ' + str(arbitrage) + '%')
+
+		#Find minimum order size
+		tradesize = min(sellbook, buybook)
+
+		#Setting order size incase balance not enough
+		if (_srcBalance < tradesize):
+			tradesize = _srcBalance
+
+		if ((tradesize*_ask) > _buyBalance):
+			tradesize = _buyBalance / _ask
+
+		#Check if above min order size
+		if ((tradesize*_bid)>0.0005001):
+			print("Selling {0} {1} @ {2} @ {3} and buying {4} {5} @ {6} @ {7}".format(tradesize, targetCurrency, sellExchangeString, _bid, tradesize, targetCurrency, buyExchangeString, _ask))
+			#Execute order
+			if not args.dryrun:
+				if (_buyExchange == 0):
+					bittrexAPI.selllimit(bittrexPair, tradesize, _bid)
+					orderNumber = poloniexAPI.buy(poloniexPair, _ask, tradesize)
+				elif (_buyExchange == 1):
+					bittrexAPI.buylimit(bittrexPair, tradesize, _ask)
+					orderNumber = poloniexAPI.sell(poloniexPair, _bid, tradesize)
+			else:
+				print("Dryrun: skipping order")
+		else:
+			print("Order size not above min order size, no trade was executed")
 
 	while True:
 
 		#Poloniex Prices
 		currentValues = poloniexAPI.api_query("returnTicker")
-		poloBid = float(currentValues[pair]["highestBid"])
-		poloAsk = float(currentValues[pair]["lowestAsk"])
+		poloBid = float(currentValues[poloniexPair]["highestBid"])
+		poloAsk = float(currentValues[poloniexPair]["lowestAsk"])
 		print("Bid @ Poloniex:	" + str(poloBid))
 		print("Ask @ Poloniex:	" + str(poloAsk))
 
 		#Bittrex Prices
-		summary=bittrexAPI.getmarketsummary(market)
+		summary=bittrexAPI.getmarketsummary(bittrexPair)
 		bittrexAsk = summary[0]['Ask']
 		print("Ask @ Bittrex:	" + str(bittrexAsk))
 		bittrexBid = summary[0]['Bid']
@@ -53,81 +101,26 @@ def main(argv):
 		# Get Balance Information, fake numbers if dryrun.
 		if not args.dryrun:
 			# Query Bittrex API
-			bittrexBalance=bittrexAPI.getbalance(currency)
-			bittrexBTCBalance=bittrexAPI.getbalance("btc")
+			bittrexTargetBalance = bittrexAPI.getbalance(targetCurrency)
+			bittrexBaseBalance = bittrexAPI.getbalance(baseCurrency)
 			# Query Poloniex API
-			allpolobalance=poloniexAPI.api_query('returnBalances')
+			allpolobalance = poloniexAPI.api_query('returnBalances')
 			# Copy Poloniex Balance Variables
-			poloniexBalance=allpolobalance[currency]
-			poloniexBTCBalance=allpolobalance["BTC"]
+			poloniexTargetBalance = allpolobalance[targetCurrency]
+			poloniexBaseBalance = allpolobalance[baseCurrency]
 		else:
 			# Faking Balance Numbers for Dryrun Simulation
-			bittrexBalance=100.0
-			bittrexBTCBalance=100.0
-			poloniexBalance=100.0
-			poloniexBTCBalance=100.0
+			bittrexTargetBalance=100.0
+			bittrexBaseBalance=100.0
+			poloniexTargetBalance=100.0
+			poloniexBaseBalance=100.0
 
 		#Buy from Polo, Sell to Bittrex
 		if (poloAsk<bittrexBid):
-			arbitrage=bittrexBid/poloAsk
-			#Check if min arb is met
-			if ((arbitrage*100)>minArb):
-				print("Buy from poloAsk, sell to bittrexBid. Profit: " + str(arbitrage*100))
-				sellbook=poloniexAPI.returnOrderBook(pair)["asks"][0][1]
-				buybook=bittrexAPI.getorderbook(market, "sell")[0]["Quantity"]
-
-				#Find minimum order size
-				tradesize=min(sellbook, buybook)
-
-				#Setting order size incase balance not enough
-				if (bittrexBalance<tradesize):
-					tradesize=bittrexBalance
-
-				if ((tradesize*poloAsk)>poloniexBTCBalance):
-					tradesize=poloniexBTCBalance/poloAsk
-
-				#Check if above min order size
-				if ((tradesize*bittrexBid)>0.0005001):
-					print("Selling {0} {1} @ BittrexBid @ {2} and buying {3} {4} @ PoloAsk @ {5}".format(tradesize, currency, bittrexBid, tradesize, currency, poloAsk))
-					#Execute order
-					if not args.dryrun:
-						bittrexAPI.selllimit(market, tradesize, bittrexBid)
-						orderNumber=poloniexAPI.buy(pair, poloAsk, tradesize)
-					else:
-						print("Dryrun: skipping order")
-				else:
-					print("Order size not above min order size, no trade was executed"
-
+			trade(0, poloAsk, bittrexBid, bittrexTargetBalance, poloniexBaseBalance)
 		#Sell to polo, Buy from Bittrex
 		elif(bittrexAsk<poloBid):
-			arbitrage=poloBid/bittrexAsk
-			#Check if min arb is met
-			if ((arbitrage*100)>minArb):
-				print("Buy from Bittrex Ask, sell to poloBid. Profit: " + str(arbitrage*100))
-				buybook=poloniexAPI.returnOrderBook(pair)["bids"][0][1]
-				sellbook=bittrexAPI.getorderbook(market, "sell")[0]["Quantity"]
-
-				#Find minimum order size
-				tradesize=min(sellbook, buybook)
-
-				#Setting order size incase balance not enough
-				if (poloniexBalance<tradesize):
-					tradesize=poloniexBalance
-
-				if((tradesize*bittrexAsk)>bittrexBTCBalance):
-					tradesize=bittrexBTCBalance/bittrexAsk
-
-				#Check if above min order size
-				if ((tradesize*bittrexAsk)>0.0005001):
-					print("Selling {0} {1} @ PoloBid @ {2} and Buying {3} {4} @ BittrexAsk @ {5}".format(tradesize, currency, poloBid, tradesize, currency, bittrexAsk))
-					#Execute order
-					if not args.dryrun:
-						bittrexAPI.buylimit(market, tradesize, bittrexAsk)
-						orderNumber=poloniexAPI.sell(pair, poloBid, tradesize)
-					else:
-						print("Dryrun: skipping order")
-				else:
-					print("Order size not above min order size, no trade was executed"
+			trade(1, bittrexAsk, poloBid, bittrexBaseBalance, poloniexTargetBalance)
 
 		time.sleep(period)
 
