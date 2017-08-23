f763431fae838dfbd814b1c960f26d51a2a411c5
MrBobBarker <MrBobBarker@users.noreply.github.com>
2017-08-23 03:20:48 -0700

    cleanup and more parser options

--- tradingbot.py
+++ tradingbot.py
@@ -7,41 +7,46 @@ import sys
 from poloniex import poloniex
 from bittrex import bittrex
 def main(argv):
-	programName = 'ArbBot'
-	logFilename = 'arbbot.log'
-
-	# create logger
-	logger = logging.getLogger(programName)
-	logger.setLevel(logging.DEBUG)
-	# create console handler and set level to debug
-	ch = logging.StreamHandler()
-	ch.setLevel(logging.DEBUG)
-	# create file handler and set level to debug
-	fh = logging.FileHandler(logFilename, mode='a', encoding=None, delay=False)
-	fh.setLevel(logging.DEBUG)
-	# create formatter
-	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-	# add formatter to ch
-	ch.setFormatter(formatter)
-	fh.setFormatter(formatter)
-	# add ch to logger
-	logger.addHandler(ch)
-	logger.addHandler(fh)
+	# User Settings
+	poloniexKey = 'POLONIEX_API_KEY'
+	poloniexSecret = 'POLONIEX_API_SECRET'
+	bittrexKey = 'BITTREX_API_KEY'
+	bittrexSecret = 'BITTREX_API_SECRET'
 
 	# Setup Argument Parser
 	parser = argparse.ArgumentParser(description='Poloniex/Bittrex Arbitrage Bot')
 	parser.add_argument('-s', '--symbol', default='XMR', type=str, required=False, help='symbol of your target coin [default: XMR]')
+	parser.add_argument('-b', '--basesymbol', default='BTC', type=str, required=False, help='symbol of your base coin [default: BTC]')
 	parser.add_argument('-r', '--rate', default=1.0, type=float, required=False, help='minimum price difference [default: 1.0]')
 	parser.add_argument('-i', '--interval', default=1, type=int, required=False, help='seconds to sleep between loops [default: 1]')
+	parser.add_argument('-l', '--logfile', default='arbbot.log', type=str, required=False, help='file to output log data to [default: arbbot.log]')
 	parser.add_argument('-d', '--dryrun', action='store_true', required=False, help='simulates without trading (API keys not required)')
 	parser.add_argument('-v', '--verbose', action='store_true', required=False, help='enables extra console messages (for debugging)')
 	args = parser.parse_args()
 
-	# Settings
-	targetCurrency = args.symbol
-	baseCurrency = 'BTC'
+	# Create Logger
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+	# Create console handler and set level to debug
+	ch = logging.StreamHandler()
+	ch.setLevel(logging.DEBUG)
+	# Create file handler and set level to debug
+	fh = logging.FileHandler(args.logfile, mode='a', encoding=None, delay=False)
+	fh.setLevel(logging.DEBUG)
+	# Create formatter
+	formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+	# Add formatter to handlers
+	ch.setFormatter(formatter)
+	fh.setFormatter(formatter)
+	# Add handlers to logger
+	logger.addHandler(ch)
+	logger.addHandler(fh)
 
-	# Print Settings
+	# Load Configuration
+	targetCurrency = args.symbol
+	baseCurrency = args.basesymbol
+
+	# Log Startup Settings
 	logger.info('Arb Pair: {}/{} | Rate: {} | Interval: {}'.format(baseCurrency, targetCurrency, args.rate, args.interval))
 	if args.dryrun:
 		logger.info("Dryrun Mode Enabled (will not trade)")
@@ -50,10 +55,9 @@ def main(argv):
 	bittrexPair = '{0}-{1}'.format(baseCurrency, targetCurrency)
 	poloniexPair = '{0}_{1}'.format(baseCurrency, targetCurrency)
 
-	#Bittrex API Keys
-	bittrexAPI = bittrex('APIKEY','APISECRET')
-	#Polo API Keys
-	poloniexAPI = poloniex('APIKEY','APISECRET')
+	# Create Exchange API Objects
+	bittrexAPI = bittrex(poloniexKey, poloniexSecret)
+	poloniexAPI = poloniex(bittrexKey, bittrexSecret)
 
 	# Trade Function
 	def trade(_buyExchange, _ask, _bid, _srcBalance, _buyBalance):
@@ -61,11 +65,10 @@ def main(argv):
 		# 0 = Poloniex
 		# 1 = Bittrex
 		arbitrage = _bid/_ask
-		# Return minumum arbitrage percentage is not met
+		# Return if minumum arbitrage percentage is not met
 		if (arbitrage <= args.rate):
 			return
-
-		if (_buyExchange == 0):
+		elif (_buyExchange == 0):
 			buyExchangeString = 'Poloniex'
 			sellExchangeString = 'Bittrex'
 			sellbook = poloniexAPI.returnOrderBook(poloniexPair)["asks"][0][1]
@@ -102,7 +105,7 @@ def main(argv):
 			else:
 				logger.info("Dryrun: skipping order")
 		else:
-			logger.info("Order size not above min order size, no trade was executed")
+			logger.warning("Order size not above min order size, no trade was executed")
 
 	# Main Loop
 	while True:
